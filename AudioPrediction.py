e635c115fb683a13b8ee0dad83be38700d259e53
Igor <silveira_igor@hotmail.com>
2026-08-10 12:59:04 +0000

    Adicionando a rede neural

--- AudioPrediction.py
+++ AudioPrediction.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import numpy as np
 from pathlib import Path
 import librosa
 import panns_inference
@@ -42,15 +43,92 @@ def gerar_tensores(lista_label_0, lista_label_1):
         embeddings = torch.cat([embeddings, torch.from_numpy(embedding)], dim=0)
     Y.extend([0]*len(lista_label_0))
     Y.extend([1]*len(lista_label_1))
+    Y = np.array(Y)
     return embeddings, Y
 
+# Define the Neural Network model
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)  # Input layer to hidden layer
+        self.relu = nn.ReLU()                          # Activation function
+        self.fc2 = nn.Linear(hidden_size, output_size) # Hidden layer to output layer
+        self.sigmoid = nn.Sigmoid()                    # Sigmoid for binary classification
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
 
 
+def test_acc_model(Y_hat, Y):
+  discretized_outputs = (Y_hat >= 0.5).float()
+  # Compare with the Y_tensor and count where they are equal
+  matches = (discretized_outputs == Y).sum().item()
+  total_elements = Y.numel()
+  #print(f"Número de correspondências: {matches}")
+  #print(f"Número total de elementos: {total_elements}")
+  acc = matches / total_elements * 100
+  #print(f"Porcentagem de correspondências (Accuracy): {acc:.2f}%")
+  return acc
+
+def train_model(model, X_data, Y_data, X_teste, Y_teste, epochs=10):
+  # Convert numpy arrays to PyTorch tensors
+  acc_treino, acc_teste = [], []
+  X_tensor = X_data #torch.from_numpy(X_data).float()
+  Y_tensor = torch.from_numpy(Y_data).float().view(-1, 1) # Reshape Y to (N, 1) for MSE loss
+  X_teste_tensor = X_teste#torch.from_numpy(X_teste).float()
+  Y_teste_tensor = torch.from_numpy(Y_teste).float().view(-1, 1) # Reshape Y to (N, 1) for MSE loss
+  # Define Loss and Optimizer
+  criterion = nn.MSELoss() # Mean Squared Error Loss
+  optimizer = optim.Adam(model.parameters(), lr=0.001) # Adam optimizer with a learning rate
+  optimizer.zero_grad() # Clear gradients
+  with torch.no_grad():
+    outputs = model(X_tensor)
+    acc = test_acc_model(outputs, Y_tensor)
+    acc_treino.append(acc)
+    outputs2 = model(X_teste_tensor)
+    acc = test_acc_model(outputs2, Y_teste_tensor)
+    acc_teste.append(acc)
+  optimizer.zero_grad() # Clear gradients
+  #print("Starting model training...")
+  for epoch in range(epochs):
+    # Forward pass
+    outputs = model(X_tensor)
+    #print(outputs.shape)
+    #print(Y_tensor.shape)
+    loss = criterion(outputs, Y_tensor)
+    # Backward and optimize
+    loss.backward()       # Backpropagation
+    optimizer.step()      # Update weights
+    with torch.no_grad():
+      outputs = model(X_tensor)
+      acc = test_acc_model(outputs, Y_tensor)
+      acc_treino.append(acc)
+      outputs2 = model(X_teste_tensor)
+      acc = test_acc_model(outputs2, Y_teste_tensor)
+      acc_teste.append(acc)
+    optimizer.zero_grad() # Clear gradients
+    # if (epoch + 1) % 10 == 0: # Print loss every epoch
+    #   print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+  #print("Training complete!")
+  return acc_treino, acc_teste
+
+
+
+
+print("Gerando os tensores")
 files = get_files("../dados_spira/clean/")
 files = filtrar_audios(files, "VOWEL.wav")
 for f in files[:10]:
     print(f)
 label_0, label_1 = separar_listas(files, "IR", "PARK")
 tensores, Y = gerar_tensores(label_0, label_1)
-print(tensores.shape, len(Y))
+print("Fim da geração dos tensores: ", tensores.shape, len(Y))
 
+output_size = 1
+model = NeuralNetwork(2048, 27, output_size)
+treino, teste = train_model(model, tensores, Y, tensores, Y, epochs=10)
+print(teste)
