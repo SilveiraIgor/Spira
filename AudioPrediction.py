54d5b237990a83bae4c71ef499d261404679b770
Igor <silveira_igor@hotmail.com>
2026-08-10 12:31:30 +0000

    gerando os tensores

--- AudioPrediction.py
+++ AudioPrediction.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import librosa
 import panns_inference
 from panns_inference import AudioTagging
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 def get_files(folder_path):
     return [str(p) for p in Path(folder_path).iterdir() if p.is_file()]
@@ -26,13 +29,20 @@ def separar_listas(lista, grupo1, grupo2):
     return lista_grupo1, lista_grupo2
 
 def gerar_tensores(lista_label_0, lista_label_1):
-    for audio_paciente in lista_label_0[:1]:
+    embeddings = torch.empty((0, 2048))
+    Y = []
+    at = AudioTagging(checkpoint_path=None, device='cuda')
+    listas = lista_label_0 + lista_label_1
+    for audio_paciente in listas:
         audio_path = audio_paciente
         (audio, _) = librosa.load(audio_path, sr=32000, mono=True)
         audio = audio[None, :]  # (batch_size, segment_samples)
-        at = AudioTagging(checkpoint_path=None, device='cuda')
+        #at = AudioTagging(checkpoint_path=None, device='cuda')
         (clipwise_output, embedding) = at.inference(audio)
-    return embedding
+        embeddings = torch.cat([embeddings, torch.from_numpy(embedding)], dim=0)
+    Y.extend([0]*len(lista_label_0))
+    Y.extend([1]*len(lista_label_1))
+    return embeddings, Y
 
 
 
@@ -41,5 +51,6 @@ files = filtrar_audios(files, "VOWEL.wav")
 for f in files[:10]:
     print(f)
 label_0, label_1 = separar_listas(files, "IR", "PARK")
-print(gerar_tensores(label_0, label_1))
+tensores, Y = gerar_tensores(label_0, label_1)
+print(tensores.shape, len(Y))
 
