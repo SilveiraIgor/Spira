108aa41a9fc0d22fb087631882d4972cf29e6310
Igor <silveira_igor@hotmail.com>
2026-09-04 15:13:56 +0000

    recalculando audio features

--- egemaps.py
+++ egemaps.py
@@ -7,11 +7,13 @@ smile = opensmile.Smile(
     feature_set=opensmile.FeatureSet.eGeMAPSv02,
     feature_level=opensmile.FeatureLevel.Functionals,
 )
-files = get_files("../dados_spira/clean/")
-files = filtrar_audios(files, "PHRASE.wav")
-df = pd.DataFrame()
-for f in files:
-    y = smile.process_file(f)
-    df = pd.concat([df, y], ignore_index=True)
-print(df.shape)
-df.to_csv("egemaps-frase.csv", index=False)
+for tipo_audio in ["VOWEL", "RHYME", "PHRASE"]:
+    files = get_files("../dados_spira/clean/")
+    files = filtrar_audios(files, tipo_audio+".wav")
+    df = pd.DataFrame()
+    for f in files:
+        y = smile.process_file(f)
+        df = pd.concat([df, y], ignore_index=True)
+        df['audio_'+tipo_audio] = f
+    print(df.shape)
+    df.to_csv("egemaps-"+tipo_audio+".csv", index=False)
